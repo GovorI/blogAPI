ec198a617bcb5245fdfc0da7ad888aff921ded94
GovorI <govorip@gmail.com>
2025-03-10 15:35:33 +0300

    fix paths in settings file

--- src/settings.ts
+++ src/settings.ts
@@ -4,8 +4,8 @@ config();
 export const SETTINGS = {
   PORT: process.env.PORT || 3000,
   PATH: {
-    BLOGS: "/api/blogs",
-    POSTS: "/api/posts",
+    BLOGS: "/blogs",
+    POSTS: "/posts",
     TESTING: "/testing/all-data",
   },
   ADMIN_AUTH: "admin:qwerty",
